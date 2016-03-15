afdd6d6cb90b3dab931e8b63f483ecaa4dff8d71
lleaff <lleaff@users.noreply.github.com>
2016-03-15 15:28:45 +0100

    default to empty GameOjects

--- Assets/scripts/GridBuilder.cs
+++ Assets/scripts/GridBuilder.cs
@@ -4,7 +4,8 @@ using System.Collections;
 
 public class GridBuilder : MonoBehaviour {
 
-	public GameObject NodePrefab;
+	public string GridName = "Grid";
+	public GameObject NodePrefab = null;
 	public int GridHeight = 20;
 	public int GridWidth = 20;
 	public float CellWidth = 5f;
@@ -13,10 +14,13 @@ public class GridBuilder : MonoBehaviour {
 
 
 	void Start () {
+		if (NodePrefab == null) {
+			NodePrefab = new GameObject ();
+		}
 		Quaternion nodePrefabRotation = NodePrefab.gameObject.transform.rotation;
 		float offsetX = -((GridWidth * CellWidth  / 2) + Middle.x);
 		float offsetZ = -((GridHeight * CellHeight / 2) + Middle.z);
-		GameObject grid = new GameObject("Grid");
+		GameObject grid = new GameObject(GridName);
 
 		for (int z = 0; z < GridHeight; z++) {
 			for (int x = 0; x < GridWidth; x++) {
