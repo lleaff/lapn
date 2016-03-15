22e0c0d2f8c9bf612f78a98d7bd939e2ad77b4ca
lleaff <lleaff@users.noreply.github.com>
2016-03-15 15:01:40 +0100

    Grid builder working

--- Assets/scripts/GridBuilder.cs
+++ Assets/scripts/GridBuilder.cs
@@ -3,19 +3,24 @@ using System.Collections;
 
 public class GridBuilder : MonoBehaviour {
 
-	public Transform node;
+	public GameObject NodePrefab;
 	public int GridHeight = 20;
 	public int GridWidth = 20;
+	public float CellWidth = 5f;
+	public float CellHeight = 5f;
 	public Vector3 Middle = new Vector3(0, 0, 0);
 
 
 	void Start () {
-		float offsetX = -((GridWidth  / 2) + Middle.x);
-		float offsetZ = -((GridHeight / 2) + Middle.z);
+		float offsetX = -((GridWidth * CellWidth  / 2) + Middle.x);
+		float offsetZ = -((GridHeight * CellHeight / 2) + Middle.z);
+		GameObject grid = Instantiate(new GameObject("Grid"), Middle, Quaternion.identity) as GameObject;
 
 		for (int z = 0; z < GridHeight; z++) {
 			for (int x = 0; x < GridWidth; x++) {
-				Instantiate (node, new Vector3 (x + offsetX, Middle.y, z + offsetZ), Quaternion.identity);
+				GameObject node = Instantiate (NodePrefab, new Vector3 (x * CellWidth + offsetX, Middle.y, z * CellHeight + offsetZ), Quaternion.identity) as GameObject;
+				node.name = string.Format ("GridNode({0}-{1})", x, z);
+				node.transform.parent = grid.transform;
 			}
 		}
 	}
