f755db693794e9b6e57da7f76430e564bd497355
lleaff <lleaff@users.noreply.github.com>
2016-03-15 17:13:36 +0100

    reformat grid builder into function

--- Assets/scripts/GridBuilder.cs
+++ Assets/scripts/GridBuilder.cs
@@ -8,29 +8,42 @@ public class GridBuilder : MonoBehaviour {
 	public GameObject NodePrefab = null;
 	public int GridHeight = 20;
 	public int GridWidth = 20;
-	public float CellWidth = 5f;
-	public float CellHeight = 5f;
+	public Vector2 CellDimensions = new Vector2 (5f, 5f);
 	public Vector3 Middle = new Vector3(0, 0, 0);
 
 
 	void Start () {
-		if (NodePrefab == null) {
-			NodePrefab = new GameObject ();
-		}
-		Quaternion nodePrefabRotation = NodePrefab.gameObject.transform.rotation;
-		float offsetX = -((GridWidth * CellWidth  / 2) + Middle.x);
-		float offsetZ = -((GridHeight * CellHeight / 2) + Middle.z);
-		GameObject grid = new GameObject(GridName);
+		BuildGrid (GridName, NodePrefab, GridHeight, GridWidth, CellDimensions, Middle);
+	}
 
-		for (int z = 0; z < GridHeight; z++) {
-			for (int x = 0; x < GridWidth; x++) {
-				GameObject node = Instantiate (NodePrefab, new Vector3 (x * CellWidth + offsetX, Middle.y, z * CellHeight + offsetZ), nodePrefabRotation) as GameObject;
+	void BuildGrid(
+		string gridName = "Grid",
+		GameObject nodeObject = null,
+		int gridWidth = 20,
+		int gridHeight = 20,
+		Vector2 cellDimensions = default(Vector2),
+		Vector3 middle = default(Vector3))
+	{
+		if (nodeObject == null) {
+			nodeObject = new GameObject ();
+		}
+		Quaternion nodeObjectRotation = nodeObject.gameObject.transform.rotation;
+		float offsetX = -((gridWidth * cellDimensions.x  / 2) + middle.x);
+		float offsetZ = -((gridHeight * cellDimensions.y / 2) + middle.z);
+		GameObject grid = new GameObject(gridName);
+
+		for (int z = 0; z < gridHeight; z++) {
+			for (int x = 0; x < gridWidth; x++) {
+				GameObject node = Instantiate (
+					nodeObject,
+					new Vector3 (x * cellDimensions.x + offsetX, Middle.y, z * cellDimensions.y + offsetZ),
+					nodeObjectRotation) as GameObject;
 				node.name = string.Format ("GridNode({0}-{1})", x, z);
 				node.transform.parent = grid.transform;
 			}
 		}
 	}
-	
+
 
 	void Update () {
 	
