0686b2de268da3b237a357e8c9a4e55b62651f73
Romain RIEHLING <riehli_r@etna-alternance.net>
2016-03-15 18:25:11 +0100

    Script button

---
+++ Assets/scripts/button.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+//using System.Collections;
+
+public class button : MonoBehaviour {
+	Button myButton;
+	bool clicked = false;
+
+	void Awake()
+	{
+		myButton = GetComponent<Button>();
+		myButton.onClick.AddListener (addCarote);
+	}
+
+	void addCarote()
+	{
+		clicked = true;
+		print ("Carrroooottte");		
+	}
+		
+	void Update()
+	{
+		if (Input.GetMouseButtonUp (0) && clicked) {
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			RaycastHit hit;
+			if (Physics.Raycast (ray, out hit, 100)) {
+				print (hit.collider.name);
+			}
+			clicked = false;
+		}
+	}
+}
