538a29bfb064c6a252d85f4c0fc0214de13b4ae6
Romain RIEHLING <m1o2i333@gmail.com>
2016-03-16 19:11:27 +0100

    Bunny go on FieldOb

--- Assets/scripts/ia_bunny.cs
+++ Assets/scripts/ia_bunny.cs
@@ -3,21 +3,24 @@ using System.Collections;
 
 public class ia_bunny : MonoBehaviour {
 
-	private GameObject[] carot;
+	private GameObject carot;
 	// Use this for initialization
 	void Start () {
-	
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (this.transform.position.x < 16)
-			this.transform.Translate (Vector3.right * 5.0f * Time.deltaTime);
-		else if (this.transform.position.x > 16)
-			this.transform.Translate (Vector3.left * 5.0f * Time.deltaTime);
-		if (this.transform.position.z < -3)
-			this.transform.Translate (Vector3.forward * 5.0f * Time.deltaTime);
-		else if (this.transform.position.z > -3)
-			this.transform.Translate (Vector3.back * 5.0f * Time.deltaTime);
+
+		if (carot = GameObject.Find ("FieldOb(Clone)")) {
+			if (this.transform.position.x < carot.transform.position.x)
+				this.transform.Translate (Vector3.right * 5.0f * Time.deltaTime);
+			else if (this.transform.position.x > carot.transform.position.x)
+				this.transform.Translate (Vector3.left * 5.0f * Time.deltaTime);
+			if (this.transform.position.z < carot.transform.position.z)
+				this.transform.Translate (Vector3.forward * 5.0f * Time.deltaTime);
+			else if (this.transform.position.z > carot.transform.position.z)
+				this.transform.Translate (Vector3.back * 5.0f * Time.deltaTime);
+		}
 	}
 }
