bbadd5e89385f10de3ff544e7120d37dd7713682
Romain RIEHLING <m1o2i333@gmail.com>
2016-03-16 19:14:56 +0100

    Bunny go on FieldOb

--- Assets/scripts/ia_bunny.cs
+++ Assets/scripts/ia_bunny.cs
@@ -4,12 +4,9 @@ using System.Collections;
 public class ia_bunny : MonoBehaviour {
 
 	private GameObject carot;
-	// Use this for initialization
 	void Start () {
 
 	}
-	
-	// Update is called once per frame
 	void Update () {
 
 		if (carot = GameObject.Find ("FieldOb(Clone)")) {
