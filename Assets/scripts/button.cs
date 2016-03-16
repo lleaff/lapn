3a987a4cc08230ee909aaf39e90c92cdf482e4f1
Romain RIEHLING <m1o2i333@gmail.com>
2016-03-16 08:58:46 +0100

    Color hover

--- Assets/scripts/button.cs
+++ Assets/scripts/button.cs
@@ -1,9 +1,15 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
 
 public class button : MonoBehaviour {
-	Button myButton;
-	bool clicked = false;
+	private Button myButton;
+	private bool clicked = false;
+	private GameObject h;
+	private Renderer[] r;
+	private GameObject old = null;
+	public Color highlight = new Color(159,159,159,0.4F);
+
 	void Awake()
 	{
 		myButton = GetComponent<Button>();
@@ -15,16 +21,27 @@ public class button : MonoBehaviour {
 		clicked = true;
 		print ("Carrroooottte");		
 	}
-		
-	void Update()
-	{
+
+	void FixedUpdate() {
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		RaycastHit hit;
 		if (Input.GetMouseButtonUp (0) && clicked) {
-			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
-			RaycastHit hit;
 			if (Physics.Raycast (ray, out hit, 100)) {
 				print (hit.collider.name);
 			}
 			clicked = false;
+		}
+		if (Physics.Raycast (ray, out hit, 100)) {
+			h = GameObject.Find (hit.collider.name);
+			r = h.GetComponents<Renderer>();
+			r[0].material.color = highlight;
+			if (old != h) {
+				if (old) {
+					r = old.GetComponents<Renderer> ();
+					r [0].material.color = Color.white;
+				}
+				old = h;
+			}
 		}
 	}
 }
