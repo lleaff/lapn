3a7d929910d4d34155855060743dc62f2058fc77
Romain RIEHLING <m1o2i333@gmail.com>
2016-03-16 19:36:17 +0100

    Rabbit eat carot

--- Assets/scripts/ia_bunny.cs
+++ Assets/scripts/ia_bunny.cs
@@ -18,6 +18,11 @@ public class ia_bunny : MonoBehaviour {
 				this.transform.Translate (Vector3.forward * 5.0f * Time.deltaTime);
 			else if (this.transform.position.z > carot.transform.position.z)
 				this.transform.Translate (Vector3.back * 5.0f * Time.deltaTime);
+		
+			if (this.transform.position.x >= carot.transform.position.x - 1 && this.transform.position.x <= carot.transform.position.x + 1 && this.transform.position.z >= carot.transform.position.z - 1 && this.transform.position.z <= carot.transform.position.z + 1) {
+				print ("On est dessus chef");
+				GameObject.Destroy (carot);
+			}
 		}
 	}
 }
