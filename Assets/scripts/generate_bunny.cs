511fe4a7db9a300fce30b461d03d6fd3034a10dc
Romain RIEHLING <m1o2i333@gmail.com>
2016-03-16 18:00:40 +0100

    [Max|min]Sec in generateBunny

--- Assets/scripts/generate_bunny.cs
+++ Assets/scripts/generate_bunny.cs
@@ -6,6 +6,8 @@ public class generate_bunny : MonoBehaviour {
 	private bool canGen = true;
 	public GameObject rabbits;
 	public GameObject[] pos;
+	public int minSec;
+	public int maxSec;
 
 	// Use this for initialization
 	void Start () {
@@ -14,7 +16,7 @@ public class generate_bunny : MonoBehaviour {
 
 	IEnumerator GenerateBunny()
 	{
-		yield return new WaitForSeconds(Random.Range(0.5f,2.5f));
+		yield return new WaitForSeconds(Random.Range(minSec,maxSec));
 		int selectPos = Random.Range (0, pos.Length);
 		Instantiate (rabbits, pos[selectPos].transform.position, rabbits.transform.localRotation);
 		canGen = true;
