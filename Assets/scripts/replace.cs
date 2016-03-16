773cfc2b4a5183c52ddf5649e72bd01c10d33906
Romain RIEHLING <m1o2i333@gmail.com>
2016-03-16 11:55:24 +0100

    Replace texture

---
+++ Assets/scripts/replace.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class replace : MonoBehaviour {
+
+	private Button myButton;
+	private bool clicked = false;
+	private GameObject h;
+	private GameObject old = null;
+	private GameObject tmp;
+	private GameObject ttmp;
+	private string name;
+
+	public GameObject field;
+
+	void Awake()
+	{
+		myButton = GetComponent<Button>();
+		myButton.onClick.AddListener (addCarote);
+	}
+
+	void addCarote()
+	{
+		if (!clicked)
+			clicked = true;
+		else {
+			if (old) {
+				GameObject.Destroy (old.transform.GetChild (0).gameObject);
+				old = null;
+			}
+			clicked = false;
+		}
+	}
+
+	void FixedUpdate() {
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		RaycastHit hit;
+		if (Input.GetMouseButtonUp (0) && clicked) {
+			if (Physics.Raycast (ray, out hit, 100) & old) {
+				name = old.name;
+				GameObject.Destroy (old.transform.GetChild (0).gameObject);
+				ttmp = Instantiate (field);
+				ttmp.transform.parent = old.transform.parent;
+				ttmp.transform.localRotation = old.transform.localRotation;
+				ttmp.transform.localPosition = old.transform.localPosition;
+				ttmp.transform.localScale = old.transform.localScale;
+				ttmp.name = name;
+				GameObject.Destroy (old);
+				old = null;
+			}
+			clicked = false;
+		}
+		if (Physics.Raycast (ray, out hit, 100) && clicked) {
+			h = GameObject.Find (hit.collider.name);
+			if (h.transform.childCount == 0) {
+				tmp = Instantiate (field);
+				tmp.transform.parent = h.transform;
+				tmp.transform.localRotation = Quaternion.identity;
+				tmp.transform.localPosition = Vector3.zero;
+				tmp.transform.localScale = Vector3.one;
+				if (old != h) {
+					if (old)
+						GameObject.Destroy (old.transform.GetChild (0).gameObject);
+					old = h;
+				}
+			}
+		}
+	}
+}
