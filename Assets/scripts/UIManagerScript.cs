61bf331bab04c63fd0bea6ca8ffcb941da6acc49
amira_s <asdfsdf@sdf.com>
2016-03-17 19:45:19 +0100

    update game menu

--- Assets/scripts/UIManagerScript.cs
+++ Assets/scripts/UIManagerScript.cs
@@ -6,13 +6,20 @@ public class UIManagerScript : MonoBehaviour {
 
 	public Animator startButton;
 	public Animator optionsButton;
+	public Animator dialog;
 
 	public void StartGame() {
 		SceneManager.LoadScene("mezon");
 	}
 
 	public void OpenSettings() {
-		startButton.SetBool("isHidden", true);
+		startButton.SetBool("isHidden", false);
 		optionsButton.SetBool("isHidden", true);
+		dialog.SetBool("isHidden", false);
+	}
+	public void CloseSettings() {
+		startButton.SetBool("isHidden", true);
+		optionsButton.SetBool("isHidden", false);
+		dialog.SetBool("isHidden", true);
 	}
 }
