6fffacc592f0fe2a0b64ac1d359612716f7002c1
Romain RIEHLING <m1o2i333@gmail.com>
2016-03-18 17:00:09 +0100

    Show ui vendage carote

---
+++ Assets/scripts/show_ui_sell.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class show_ui_sell : MonoBehaviour {
+
+	public GameObject ui_sell;
+	private Button myButton;
+	static private bool is_active = false;
+
+	void Awake()
+	{
+		ui_sell.SetActive (false);
+		myButton = GetComponent<Button> ();
+		myButton.onClick.AddListener (show);
+	}
+
+	void show()
+	{
+		if (is_active == true) {
+			ui_sell.SetActive (false);
+			is_active = false;
+		} else {
+			ui_sell.SetActive (true);
+			is_active = true;	
+		}
+	}
+}
