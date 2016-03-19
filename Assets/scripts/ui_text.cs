739dee4c351ba7212b212d3e642184639379e36d
Romain RIEHLING <m1o2i333@gmail.com>
2016-03-19 17:52:19 +0100

    display time

--- Assets/scripts/ui_text.cs
+++ Assets/scripts/ui_text.cs
@@ -9,16 +9,31 @@ public class ui_text : MonoBehaviour {
 	public Text time;
 	public Text money;
 	private float m_time;
+	private int sec;
 
 	void Start () {
+		m_time = 0;
+		sec = 0;
 		globalValue = GameObject.Find ("GlobalValue");
 	}
 		
 	void Update () {
-		
 		m_time += Time.deltaTime;
+		if (m_time >= 1) {
+			m_time = 0;
+			sec += 1;
+		}
 		carrot.text = globalValue.GetComponent<globalValue> ().Carrots.ToString ();
-		time.text = m_time.ToString ();
+		if (sec / 60 < 10)
+			time.text = "0" + (sec / 60).ToString ();
+		else
+			time.text = (sec / 60).ToString ();
+		time.text += ":";
+		if (sec % 60 < 10)
+			time.text += "0" + (sec % 60).ToString ();
+		else
+			time .text+= (sec % 60).ToString ();
+
 		money.text = globalValue.GetComponent<globalValue> ().Money.ToString();
 	}
 }
