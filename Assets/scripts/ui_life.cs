b230b81461a5344971c6aeaaa311474b13dd8ece
Romain RIEHLING <m1o2i333@gmail.com>
2016-03-19 12:04:51 +0100

    Input key for life

--- Assets/scripts/ui_life.cs
+++ Assets/scripts/ui_life.cs
@@ -8,23 +8,33 @@ public class ui_life : MonoBehaviour {
 	public int index;
 	private GameObject globalValue;
 	private Transform image;
+	private KeyCode[] keys;
 
 	void Awake()
 	{
 		globalValue = GameObject.Find ("GlobalValue");
 		myButton = GetComponent<Button> ();
 		myButton.onClick.AddListener (eat);
+		keys = new KeyCode[4];
+		keys [0] = KeyCode.Alpha1;
+		keys [1] = KeyCode.Alpha2;
+		keys [2] = KeyCode.Alpha3;
+		keys [3] = KeyCode.Alpha4;
+	    
 	}
 
 	void Update () {
 		image = this.transform.GetChild (0);
 		image.localScale = new Vector3 (1, globalValue.GetComponent<globalValue> ().get_life (index) * 0.01F, 1);
+		if (Input.GetKey(keys	[index]))
+			eat();
 	}
 
 	void eat()
 	{
-		globalValue.GetComponent<globalValue> ().remove_carrots (5);
-		globalValue.GetComponent<globalValue> ().add_life (5, index);
+		if (globalValue.GetComponent<globalValue> ().get_carrots () >= 5) {
+			globalValue.GetComponent<globalValue> ().remove_carrots (5);
+			globalValue.GetComponent<globalValue> ().add_life (5, index);
+		}
 	}
-
 }
