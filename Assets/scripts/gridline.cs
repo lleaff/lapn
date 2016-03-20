215512664830678499b9946de04bde067cdde4c3
vanner-a <vanner_a@etna-alternance.net>
2016-03-20 13:48:56 +0100

    grid cleanup

--- Assets/scripts/gridline.cs
+++ Assets/scripts/gridline.cs
@@ -11,30 +11,10 @@ public class gridline : MonoBehaviour {
 
 	private Material lineMaterial;
 
-	private Color mainColor = new Color(1f,1f,1f,1f);
-		
-	void CreateLineMaterial() 
-	{
-
-		if( !lineMaterial ) {
-			lineMaterial = new Material( "Shader \"Lines/Colored Blended\" {" +
-				"SubShader { Pass { " +
-				"    Blend SrcAlpha OneMinusSrcAlpha " +
-				"    ZWrite Off Cull Off Fog { Mode Off } " +
-				"    BindChannels {" +
-				"      Bind \"vertex\", vertex Bind \"color\", color }" +
-				"} } }" );
-			lineMaterial.hideFlags = HideFlags.HideAndDontSave;
-			lineMaterial.shader.hideFlags = HideFlags.HideAndDontSave;
-		}
-	}
+	private Color mainColor = new Color(0f,0f,0f,1f);
 
 	void OnPostRender() 
 	{        
-		CreateLineMaterial();
-		// set the current material
-		lineMaterial.SetPass( 0 );
-
 		GL.Begin( GL.LINES );
 		GL.Color(mainColor);
 
