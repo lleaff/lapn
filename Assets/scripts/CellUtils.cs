47301ad4a66f5236fff32026744f29a5f4c580cf
lleaff <lleaff@users.noreply.github.com>
2016-03-22 13:02:01 +0100

    Recolter base

---
+++ Assets/scripts/CellUtils.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class CellUtils {
+	
+	public static bool HasObjectWithTag(GameObject cell, string tagName) {
+		foreach (Transform child in cell.transform) {
+			if (child.CompareTag (tagName)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public static GameObject FindObjectWithTag(GameObject cell, string tagName) {
+		foreach (Transform child in cell.transform) {
+			if (child.CompareTag (tagName)) {
+				return child.gameObject;
+			}
+		}
+		return null;
+	}
+}
--- Assets/scripts/Recolter.cs
+++ Assets/scripts/Recolter.cs
@@ -1,15 +1,50 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Recolter : MonoBehaviour {
+public class Recolter : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-	
+	public bool Selected = false;
+
+
+	void Start ()
+	{
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	void Update ()
+	{
+		if (!Selected) {
+			return;
+		}
+
+		bool clicked = Input.GetMouseButtonUp (0);
+
+		GameObject cell = getPointedObj ();
+		if (!cell) {
+			return;
+		}
+
+		GameObject carrots = CellUtils.FindObjectWithTag (cell, "Carrot");
+		if (!carrots) {
+			return;
+		}
+
+		if (clicked) {
+			Harvest (cell, carrots);
+		}
+	}
+
+	bool Harvest (GameObject cell, GameObject carrots) {
+		return true;
+	}
+
+	GameObject getPointedObj ()
+	{
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		RaycastHit hit;
+		if (Physics.Raycast (ray, out hit, 100, 1 << LayerMask.NameToLayer ("PlacementGrid"))) {
+			return hit.collider.gameObject;
+		}
+		return null;
 	}
 }
