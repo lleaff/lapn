1d64a880efdfa124a39e842e2b45934d7d4adade
lleaff <lleaff@users.noreply.github.com>
2016-03-22 19:12:44 +0100

    fix

---
+++ Assets/scripts/WeatherManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeatherManager : MonoBehaviour {
+
+	public static WeatherManager i = null; /* WeatherManager instance */
+
+	public float Heat = 20;
+	public float Humidity = 1; /* 1 == neutral */
+
+	void Awake()
+	{
+		if (i == null) {
+			i = this;
+		} else if (i != this) {
+			Destroy (gameObject);
+		}
+		DontDestroyOnLoad (gameObject);
+	}
+}
