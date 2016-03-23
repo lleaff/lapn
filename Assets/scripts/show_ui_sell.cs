1c517d0d23b6e20a2a267a8e8f2c296d930263d3
Romain RIEHLING <m1o2i333@gmail.com>
2016-03-23 15:45:04 +0100

    ui_sell now useless

--- Assets/scripts/show_ui_sell.cs
+++ Assets/scripts/show_ui_sell.cs
@@ -6,17 +6,8 @@ using System.Collections.Generic;
 
 public class show_ui_sell : MonoBehaviour {
 
-	private Button myButton;
 	private List<int>list_value = new List<int>();
 	public Texture line;
-	public GameObject ui_sell;
-
-	void Awake ()
-	{
-		ui_sell.SetActive (false);
-		myButton = GetComponent<Button> ();
-		myButton.onClick.AddListener (show);
-	}
 
 	void Update()
 	{
@@ -38,15 +29,6 @@ public class show_ui_sell : MonoBehaviour {
 		foreach (int price in list_value) {
 			GUI.DrawTexture (new Rect (25F + ((100F / list_value.Count) * i) , 575F - (600 - Screen.height) - price * (100 / get_max()), 100F / list_value.Count, price * (100 / get_max())), line);
 			i++;
-		}
-	}
-
-	public void show()
-	{
-		if (ui_sell.activeInHierarchy) {
-			ui_sell.SetActive (false);
-		} else {
-			ui_sell.SetActive (true);	
 		}
 	}
 }
