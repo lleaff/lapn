672067c2c9f260917037385d0b33d6c8e5a900e5
Romain RIEHLING <m1o2i333@gmail.com>
2016-03-23 19:56:25 +0100

    time

--- Assets/scripts/pause.cs
+++ Assets/scripts/pause.cs
@@ -4,16 +4,50 @@ using System.Collections;
 
 public class pause : MonoBehaviour {
 
+	private Button myButton;
+	public int index;
+	public GameObject play;
+	public GameObject Pause;
+	public GameObject speed;
+	public GameObject normal;
+
+	void Awake(){
+		myButton = GetComponent<Button> ();
+		if (index == 0)
+			myButton.onClick.AddListener (time_0);
+		else if (index == 1)
+			myButton.onClick.AddListener (time_1);
+		else if (index == 2)
+			myButton.onClick.AddListener (time_2);
+		else if (index == 3)
+			myButton.onClick.AddListener (time_3);
+	}
+
 	public void time_0() {
 		Time.timeScale = 0;
+		Pause.SetActive (false);
+		play.SetActive (true);
 	}
 
 	public void time_1() {
 		Time.timeScale = 1;
+		Pause.SetActive (true);
+		play.SetActive (false);
 	}
 
 	public void time_2() {
+		speed.SetActive (false);
+		normal.SetActive (true);
+		Pause.SetActive (true);
+		play.SetActive (false);
 		Time.timeScale = 2;
 	}
 
+	public void time_3() {
+		speed.SetActive (true);
+		normal.SetActive (false);
+		Pause.SetActive (true);
+		play.SetActive (false);
+		Time.timeScale = 1;
+	}
 }
