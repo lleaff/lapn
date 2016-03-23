a95d04ce99a8e9403d94d3892859d705f3c7d0b7
vanner-a <vanner_a@etna-alternance.net>
2016-03-23 17:09:14 +0100

    trap price correction

--- Assets/scripts/pause.cs
+++ Assets/scripts/pause.cs
@@ -4,6 +4,13 @@ using System.Collections;
 
 public class pause : MonoBehaviour {
 
+	private Button my_button;
+
+	void Awake () {
+		my_button = GetComponent<Button> ();
+		my_button.onClick.AddListener (time_2);
+	}
+
 	public void time_0() {
 		Time.timeScale = 0;
 	}
