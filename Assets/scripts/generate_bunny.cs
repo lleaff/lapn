b0b67e5f77f881fdf37156c4817399c7e1a70f86
Romain RIEHLING <m1o2i333@gmail.com>
2016-03-24 13:18:40 +0100

    Father's bonus

--- Assets/scripts/generate_bunny.cs
+++ Assets/scripts/generate_bunny.cs
@@ -6,6 +6,7 @@ public class generate_bunny : MonoBehaviour {
 	private bool canGen = true;
 
 	private int start_time = 30;
+	private int spawn_delay = 0; 
 	private int nb_rabbit = 0;
 	private int nb_trap = 0;
 	private float time = 0;
@@ -27,10 +28,10 @@ public class generate_bunny : MonoBehaviour {
 
 	IEnumerator GenerateBunny()
 	{
-		if (start_time - (nb_rabbit * 0.3F + nb_trap * 0.4F + time * 0.005F) < 5)/*If time is smaller than 5s*/
+		if (spawn_delay - (nb_rabbit * 0.3F + nb_trap * 0.4F + time * 0.005F) < 5)/*If time is smaller than 5s*/
 			yield return new WaitForSeconds(5);
 		else
-			yield return new WaitForSeconds(start_time - (nb_rabbit * 0.3F + nb_trap * 0.4F + time * 0.005F));
+			yield return new WaitForSeconds(spawn_delay - (nb_rabbit * 0.3F + nb_trap * 0.4F + time * 0.005F));
 		int selectPos = Random.Range (0, pos.Length);
 		Instantiate (rabbits, pos[selectPos].transform.position, rabbits.transform.localRotation);
 		canGen = true;
@@ -40,10 +41,10 @@ public class generate_bunny : MonoBehaviour {
 	//------------------------------------------------------------
 
 	void Update () {
-		if (globals.i.List [0] > 0)/*Father's bonus: if he is alive, bunny spawn 10s later*/
-			start_time = 40;
+		if (globals.i.List [0] > 0)  /*Father's bonus: if he is alive, bunny spawn 10s later*/
+			spawn_delay = start_time + 10;
 		else
-			start_time = 30;
+			spawn_delay = 30;
 		
 		nb_rabbit = GameObject.FindGameObjectsWithTag ("Bunny").Length;
 		nb_trap = get_nbr_trap ();
