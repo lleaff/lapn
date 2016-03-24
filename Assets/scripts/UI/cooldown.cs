f15188f74214bc0e03bb62ca9bbe1ca9d03b3382
Romain RIEHLING <m1o2i333@gmail.com>
2016-03-24 11:40:17 +0100

    Sync cooldown with Time from Replace

--- Assets/scripts/UI/cooldown.cs
+++ Assets/scripts/UI/cooldown.cs
@@ -4,35 +4,15 @@ using System.Collections;
 
 public class cooldown : MonoBehaviour {
 
-	private Button myButton;
-	private float time_click;
-	private float time;
-	private bool clicked = false;
-
 	public GameObject coolDown;
-
-	private void reset()
-	{
-		if (!clicked)
-			time_click = TimeManager.i.Seconds;
-		clicked = true;
-	}
 
 	// Monobehaviour
 	//------------------------------------------------------------
 
-	void Awake() {
-		myButton = GetComponent<Button> ();
-		myButton.onClick.AddListener (reset);
-	}
-
 	void Update () {
-		time = TimeManager.i.Seconds;
-		if (time - time_click <= 5 && clicked)
-			coolDown.transform.localScale = new Vector3 (1, 1 - ((time - time_click) * 0.2F), 1);
-		else if (time - time_click > 5 && clicked)
-			clicked = false;
+		if (this.GetComponent<Replace>().Time <= 5)
+			coolDown.transform.localScale = new Vector3 (1, 1 - ((this.GetComponent<Replace>().Time) * 0.2F), 1);
 		else
-			coolDown.transform.localScale = new Vector3(1, 0 ,1);
+			coolDown.transform.localScale = new Vector3 (1, 0, 1);
 	}
 }
