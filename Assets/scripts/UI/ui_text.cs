697920a68a1fc7b20eeaf816a09888d1f63d91d7
Romain RIEHLING <m1o2i333@gmail.com>
2016-03-25 10:31:14 +0100

    weather info and not sell night

--- Assets/scripts/UI/ui_text.cs
+++ Assets/scripts/UI/ui_text.cs
@@ -7,10 +7,14 @@ public class ui_text : MonoBehaviour {
 	public Text carrot;
 	public Text time;
 	public Text money;
+	public Text temp;
+	public Text humidity;
 
 	void Update () {
 		carrot.text = globals.i.Carrots.ToString ();
 		time.text = TimeManager.i.FormattedTime;
 		money.text = globals.i.Money.ToString();
+		temp.text = WeatherManager.i.Heat.ToString("0.0") + "˚C";
+		humidity.text = WeatherManager.i.Humidity.ToString ("0.0000");
 	}
 }
